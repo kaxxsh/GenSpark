9c37aca4627da50828eae62e3fb862d1965aee05
kaxxsh <kameshwaran2002@gmail.com>
2024-04-22 13:53:19 +0530

    Added BookingRepository class responsible for managing booking-related data operations, including getting all bookings, getting a booking by ID, adding a new booking, updating an existing booking, and removing a booking by ID.
    #DataAccessLayer #BookingRepository

---
+++ Day 9/MovieBookingSystemApp/DataAccessLayer/BookingRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Models;
+
+namespace DataAccessLayer
+{
+    public class BookingRepository
+    {
+        private List<Booking> _bookings;
+
+        public BookingRepository()
+        {
+            _bookings = new List<Booking>();
+        }
+
+        // Method to get all bookings
+        public List<Booking> GetAllBookings()
+        {
+            return _bookings;
+        }
+
+        // Method to add a new booking
+        public void AddBooking(Booking booking)
+        {
+            _bookings.Add(booking);
+        }
+
+        // Method to remove a booking by its ID
+        public void RemoveBooking(int id)
+        {
+            Booking bookingToRemove = _bookings.Find(b => b.Id == id);
+            if (bookingToRemove != null)
+            {
+                _bookings.Remove(bookingToRemove);
+            }
+        }
+
+        // Method to get a booking by its ID
+        public Booking GetBookingById(int id)
+        {
+            return _bookings.Find(b => b.Id == id);
+        }
+    }
+}
