f966ea9933cb2d4bf868237c845c4fcabe74f046
kaxxsh <kameshwaran2002@gmail.com>
2024-04-22 13:44:44 +0530

    Add BookingManager class responsible for managing booking-related operations such as adding new bookings, retrieving bookings by ID, and removing bookings.

---
+++ Day 9/MovieBookingSystemApp/BusinessLogicLayer/BookingManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using DataAccessLayer;
+using Models;
+
+namespace BusinessLogicLayer
+{
+    public class BookingManager
+    {
+        private BookingRepository _bookingRepository;
+
+        public BookingManager()
+        {
+            _bookingRepository = new BookingRepository();
+        }
+
+        // Method to get all bookings
+        public List<Booking> GetAllBookings()
+        {
+            return _bookingRepository.GetAllBookings();
+        }
+
+        // Method to add a new booking
+        public void AddBooking(Booking booking)
+        {
+            _bookingRepository.AddBooking(booking);
+        }
+
+        // Method to remove a booking by its ID
+        public void RemoveBooking(int id)
+        {
+            _bookingRepository.RemoveBooking(id);
+        }
+
+        // Method to get a booking by its ID
+        public Booking GetBookingById(int id)
+        {
+            return _bookingRepository.GetBookingById(id);
+        }
+    }
+}
